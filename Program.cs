eeeec8cb4b4f29d4d907a56ec9c1ee846f784814
Mustafa Dogan <mdogan89@hotmail.com>
2024-12-26 12:45:27 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Bir sayı giriniz: ");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int top = 0;
+while (sayac<=sayi)
+{
+    top += sayac;
+    sayac++;
+}
+
+Console.WriteLine("ortalama: " + top/sayi);
+
+char c = 'a';
+while (c < 'z')
+{
+    Console.Write(c);
+    c++;
+}
+
+
+string[] arabalar = { "BMW", "Ford", "Mercedes" };
+
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
+
+
+
